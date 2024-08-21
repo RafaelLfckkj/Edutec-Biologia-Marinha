0e7e1d6a9db2f04f0e4b648dd0b5e10540eff0f2
RafaelLfckkj <142596899+RafaelLfckkj@users.noreply.github.com>
2024-08-21 01:00:47 +0000

    Correções 5

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const prevButton = document.querySelector('.seta-esquerda');
 const nextButton = document.querySelector('.seta-direita');
 const carouselImages = document.querySelector('.carroselimg');
-const carouselItems = document.querySelector('.peixes-c');
+const carouselItems = document.querySelectorAll('.peixes-c');
 let currentIndex = 0;
 
 function showSlide(index) {
-  if (index < 0) index = carouselItems.length - 1;
-  if (index >= carouselItems.length) index = 0;
+  if (index < 0);
+  if (index > carouselItems.length);
   currentIndex = index;
   carouselImages.style.transform = `translateX(-${index * 150}%)`;
+   if(currentIndex)(carouselImages.style.transform = `translateX(${index * -150}%)`);
 }
 
-prevButton.addEventListener('click', () => showSlide(currentIndex - 1));  nextButton.addEventListener('click', () => showSlide(currentIndex + 1));
+function returne(currentIndex) {
+  if (currentIndex > 4)
+  nextButton.addEventListener('click', () => showSlide(currentIndex - 4));
+  currentIndex = index
+}
+
+prevButton.addEventListener('click', () => showSlide(currentIndex - 1)); 
+nextButton.addEventListener('click', () => showSlide(currentIndex + 1));
+
+
+returne(currentIndex)
