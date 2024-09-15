7dee049a8f4827d8bde0cc71780cadfd2c647355
RafaelLfckkj <142596899+RafaelLfckkj@users.noreply.github.com>
2024-09-15 00:28:07 +0000

    MAR PROFUNDO quase finalizado

--- src/scripts/marprofundo.js
+++ src/scripts/marprofundo.js
@@ -1,8 +1,17 @@
-const depthElement = document.getElementById('depth');
+// Função para verificar a distância percorrida ao rolar
+window.addEventListener("scroll", function() {
+  // Seleciona o primeiro h1 e o main
+  const h1 = document.querySelector("h1.zone0");
+  const main = document.querySelector("main#deep-sea");
+  const counter = document.getElementById("depth");
 
-// Função para atualizar a profundidade com base no scroll
-window.addEventListener('scroll', function() {
-  let scrollPosition = window.scrollY; // Posição do scroll
-  let depth = Math.floor(scrollPosition * 1); // Aumenta a profundidade conforme o scroll
-  depthElement.textContent = depth;
+  // Obtém a distância entre o topo do h1 e o topo da janela de visualização
+  const h1Top = h1.getBoundingClientRect().top;
+  const mainBottom = main.getBoundingClientRect().bottom;
+
+  // Define a profundidade inicial quando o h1 estiver na tela
+  if (h1Top < window.innerHeight && mainBottom > 0) {
+    const depth = Math.floor((window.scrollY - h1.offsetTop) * 0.09); // Multiplica para ajustar a velocidade do contador
+    counter.textContent = Math.max(depth, 0); 
+  }
 });
