7edf2625ca7eced37cbd1fff00e0af277b275575
Rafaellfckkj <rafaxolive@gmail.com>
2025-08-11 19:46:21 -0300

    removendo o back-end

--- logout.js
+++
@@ -1,9 +0,0 @@
-export function logout() {
-    const button = document.querySelector("#logout-button");
-    button.addEventListener("click", () => {
-        localStorage.removeItem("token")
-        window.location.reload()
-    });
-
-    
-}
